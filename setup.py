81686b61fa7cca814f6492054f3b373a7e95a75b
Michael Palumbo <palumbomi95@gmail.com>
2022-10-20 10:12:52 -0400

    setting up

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import find_packages, setup
 
 setup(
     name="hmi_clean",
-    packages=find_packages(where="src"),
-    package_dir={"", "src"},
+    packages=find_packages(where="src")
 )
